c4d6332bdb9018713a0b9cc6466c0ce36b86662b
Saanvi01Mohapatra <saanvimohapatra05@gmail.com>
2024-08-28 01:06:35 +0530

    Add files via upload

---
+++ c10_2.cpp
@@ -0,0 +1,22 @@
+/*Saanvi
+23070123110
+ENTC B2
+EXCP 10 _1*/
+#include<iostream> 
+using namespace std; 
+void swap(int *x, int *y) 
+{
+    int *swap;
+    swap=x;
+    x=y;
+    y=swap;
+}
+
+int main() 
+{
+    int a=84,b=56;
+    swap(a,b);
+    cout<<"Value of a is: "<<a<<"\n";
+    cout<<"Value of b is: "<<b<<"\n";
+    return 0;
+}
---
+++ c10_3.cpp
@@ -0,0 +1,23 @@
+
+/*Saanvi
+23070123110
+ENTC B2
+EXCP 10 _3*/
+#include<iostream> 
+using namespace std; 
+void swap(int *x, int *y) 
+{
+    int swap;
+    swap=*x;
+    *x=*y;
+    *y=swap;
+}
+
+int main() 
+{
+    int a=45,b=89;
+    swap(&a,&b);
+    cout<<"Value of a is: "<<a<<"\n";
+    cout<<"Value of b is: "<<b<<"\n";
+    return 0;
+}
